448d3a1768d8e4ec4d56a9f3bfbe499926a6656a
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2026-02-12 17:17:02 +0400

    Update HomeLayout.vue

--- .vitepress/components/HomeLayout.vue
+++ .vitepress/components/HomeLayout.vue
@@ -1,17 +1,14 @@
 <script setup>
-import { computed } from 'vue'
 import { useData } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import HomePage from './HomePage.vue'
 
 const { frontmatter } = useData()
-const isHome = computed(() => frontmatter.value.layout === 'boom-home')
 
-// Получаем компонент Layout из дефолтной темы
-const DefaultLayout = DefaultTheme.Layout
+const VPLayout = DefaultTheme.Layout
 </script>
 
 <template>
-  <HomePage v-if="isHome" />
-  <DefaultLayout v-else />
+  <HomePage v-if="frontmatter.layout === 'boom-home'" />
+  <VPLayout v-else />
 </template>
