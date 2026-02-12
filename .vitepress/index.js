ea72473878345ed4a5579f405e8c3b6bceeca781
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2026-02-12 17:04:11 +0400

    Update index.js

--- .vitepress/index.js
+++ .vitepress/index.js
@@ -1,15 +1,11 @@
 // .vitepress/theme/index.js
-// Кастомная тема: расширяем дефолтную + добавляем наши стили и компоненты
-
 import DefaultTheme from 'vitepress/theme'
-import HomeLayout from './components/HomeLayout.vue'
+import HomePage from './components/HomePage.vue'
 import './boom-styles.css'
-
-// Если у тебя уже есть style.css / custom.css — оставь их:
-// import './style.css'
-// import './custom.css'
 
 export default {
   extends: DefaultTheme,
-  Layout: HomeLayout,
+  enhanceApp({ app }) {
+    app.component('HomePage', HomePage)
+  }
 }
